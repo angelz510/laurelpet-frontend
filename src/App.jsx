b4cdc8f996b4db6c09a9a107acff2ed66ba4b50f
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-03-28 15:27:16 -0700

    create folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "pages/Home/Home";
+import Login from "pages/Login/Login";
+import Profile from "pages/Profile/Profile";
 
 function App() {
   return (
