9d3b8fb5085038d437601f38d36b430cdd60a5ce
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-07-11 23:55:00 -0700

    added dropzone but not working

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   const mode = useSelector((state) => state.mode);
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
     <Box>
       <Box
         width="100%"
-        backgrounColor={theme.palette.background.alt}
+        backgroundColor={theme.palette.background.alt}
         p="1rem 6%"
         textAlign="center"
       >
@@ -22,7 +22,7 @@ const LoginPage = () => {
         p="2rem"
         m="2rem auto"
         borderRadius="1.5rem"
-        backgrounColor={theme.palette.background.alt}
+        backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Hello, welcome to the Laurel.
